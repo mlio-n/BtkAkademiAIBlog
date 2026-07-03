19b46d27074e427b64dc741d30210450103465d3
mlio-n <msariarslan23@posta.pau.edu.tr>
2026-07-03 23:28:32 +0300

    feat(entities): add Category and Product core entities

---
+++ BtkAkademiAIBlog.WebApi/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BtkAkademiAIBlog.WebApi.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
