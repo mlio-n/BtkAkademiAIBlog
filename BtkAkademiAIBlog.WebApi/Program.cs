3979c2b595046c919810b3d61f1fee07d5a7a4c5
Muzaffer Sariarslan <msariarslan23@gmail.com>
2026-07-03 18:47:12 +0300

    Proje dosyası ekle.

---
+++ BtkAkademiAIBlog.WebApi/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// .NET 10.0 ile gelen yerleşik OpenAPI servisi
+builder.Services.AddOpenApi();
+
+// --- SWAGGER İÇİN EKLENEN SERVİSLER ---
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// -------------------------------------
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    // --- SWAGGER ARAYÜZÜNÜ AKTİF EDEN ORTAM (MIDDLEWARE) ---
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    // -----------------------------------------------------
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
