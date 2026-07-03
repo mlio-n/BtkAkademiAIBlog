ef94a9a285d879602443ef10ef7bcce9f5d08dfe
mlio-n <msariarslan23@posta.pau.edu.tr>
2026-07-03 23:43:39 +0300

    feat(data): add BlogAIContext and configure EF Core SQL Server

---
+++ BtkAkademiAIBlog.WebApi/Context/BlogAIContext.cs
@@ -0,0 +1,15 @@
+﻿using BtkAkademiAIBlog.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BtkAkademiAIBlog.WebApi.Context
+{
+    public class BlogAIContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial catalog=BtkAkademiAIBlogDb;integrated security=true;TrustServerCertificate=True;Encrypt=False;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
