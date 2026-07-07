8b8f19aacbc5c8282ee06eca7d0a50388aa4f6d0
mlio-n <msariarslan23@posta.pau.edu.tr>
2026-07-07 18:08:34 +0300

    feat(webui): view component extractions
    docs(webui): finalize layout head, scripts, and right sidebar modularization steps

---
+++ BtkAkademiAIBlog.WebUI/Views/Shared/Components/_LayoutRightSideBarComponentPartial/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BtkAkademiAIBlog.WebUI.Views.Shared.Components._LayoutRightSideBarComponentPartial
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BtkAkademiAIBlog.WebUI/Views/Shared/Components/_LayoutScriptsComponentPartial/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BtkAkademiAIBlog.WebUI.Views.Shared.Components._LayoutScriptsComponentPartial
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
